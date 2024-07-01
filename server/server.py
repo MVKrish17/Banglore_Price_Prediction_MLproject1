1c050ae234f17609d3a124d7003903524796a982
MVKrish17 <vamsikrishcse2002@gmail.com>
2024-07-01 22:40:37 +0530

    recent update

--- server/server.py
+++ server/server.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 import util
 
-app = Flask(__name__)
+app = Flask(__name__ ,template_folder='templates', static_folder='static')
+@app.route('/')
+def index():
+    return render_template('app.html')
 
-
-@app.route('/get_location_names', methods=['GET'])
+@app.route('/get_location_names')
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
@@ -13,21 +15,22 @@ def get_location_names():
 
     return response
 
+
 @app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
     total_sqft = float(request.form['total_sqft'])
     location = request.form['location']
     bhk = int(request.form['bhk'])
     bath = int(request.form['bath'])
-
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
 
     return response
 
+
 if __name__ == "__main__":
-    print("Starting Python Flask Server For Home Price Prediction...")
+    print("starting python flask")
     util.load_saved_artifacts()
-    app.run()
+    app.run(host='127.0.0.1', port=5000)
--- server/util.py
+++ server/util.py
@@ -1,7 +1,6 @@
 import json
 import pickle
 import numpy as np
-
 __locations = None
 __data_columns = None
 __model = None
@@ -23,25 +22,20 @@ def get_estimated_price(location,sqft,bhk,bath):
 def get_location_names():
     return __locations
 
-def get_data_columns():
-    return __data_columns
-
 def load_saved_artifacts():
     print("loading saved artifacts..start")
     global __data_columns
     global __locations
-
+    global __model
     with open("./artifacts/columns.json", "r") as f:
         __data_columns = json.load(f)['data_columns']
         __locations = __data_columns[3:]
 
-    global __model
-    if __model is None:
-        with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
-            __model = pickle.load(f)
+    with open('./artifacts/banglore_home_prices_model.pickle', 'rb') as f:
+        __model = pickle.load(f)
     print("loading saved artifacts...done")
 
-if __name__ == '__main__':
+if __name__=="__main__":
     load_saved_artifacts()
     print(get_location_names())
     print(get_estimated_price('1st Phase JP Nagar', 1000, 3, 3))
